4a62df61ac5dcdc6fd369d7b96ab69b26f52b7ff
HamidrezaFTP <154135203+HamidrezaFTP@users.noreply.github.com>
2023-12-20 11:00:07 -0800

    Add files via upload

---
+++ TailwindCss-Landing page/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        vazir : ['Vazir']
+      },
+      colors: {
+        pink: {
+          750: '#d53569'
+        },
+        amber: {
+          350: '#daad51'
+        }
+      },
+    },
+  },
+  plugins: [],
+}
+
